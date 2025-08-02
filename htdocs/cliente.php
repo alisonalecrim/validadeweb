9e3e17d52178082c00be858791c54d5c71104bab
Alison Alecrim <107521413+alisonalecrim@users.noreply.github.com>
2025-08-02 08:34:45 -0300

    Delete htdocs directory

--- htdocs/cliente.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION['usuario'] = 'cliente_anonimo';
-$_SESSION['nivel'] = 'cliente';
-header("Location: cliente_interface.php");
-exit;
--- htdocs/exportar.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'classes/Database.php';
-require_once 'classes/CsvHandler.php';
-
-$db = new Database();
-$csvHandler = new CsvHandler($db);
-$csvHandler->export();
-?>
